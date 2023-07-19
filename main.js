e8413c5c4128fc94014908858e3b68c489d4f74a
Matthew Brozen <matthewbrozen@Matthews-MacBook-Pro-2.local>
2023-07-19 14:17:46 -0700

    animation update

--- main.js
+++ main.js
@@ -2,24 +2,6 @@ function toggleFullScreen(id) {
     document.getElementById(id).requestFullscreen()
   }
 
-//   function myFunction(){
-//     var arr = document.getElementsByClassName('rotate');
-//     for(var i = 2; i > arr.length; i--){
-//         if(arr[i].classList.contains('hide')){
-//             arr[i].classList.remove("hide");
-//         }else{
-//             arr[i].classList.add("hide");
-//         }
-//     }
-
-// }
-
-// myFunction();
-
-// setInterval(function(){
-//     myFunction()
-// }, 30000)
-
 var im = document.getElementById("img");
 
 var images = ["https://picsum.photos/seed/3/342/180","https://picsum.photos/seed/2/342/180","https://picsum.photos/seed/1/342/180"];
@@ -37,24 +19,42 @@ function changeImage()
 
 setInterval(changeImage, 6000);
 
+
+
 var im2 = document.getElementById("switchImg");
 var cat = document.getElementById("catPush");
 
-var images2 = ["https://picsum.photos/seed/3/342/180","https://picsum.photos/seed/2/342/180","https://picsum.photos/seed/1/342/180"];
-var cats = ["Directing","Perfomaning", "Fine Art"];
+var images2 = ["https://picsum.photos/seed/2/342/180","https://picsum.photos/seed/1/342/180","https://picsum.photos/seed/3/342/180",];
 var index2=0;
 
 function changeImage2()
 {
+  
   im2.setAttribute("src", images2[index2]);
-  cat.innerText=cats[index2];
   index2++;
-  if(index2 >= images2.length)
+  if(index2 >= images2.length )
   {
     index2=0;
   }
 }
 
-setInterval(changeImage2, 3000);
+setInterval(changeImage2, 5000);
+
+var cats = ["Directing","Perfomaning", "Fine Art"];
+var index3=0;
+
+
+function changeImage3()
+{
+  
+  cat.innerText=cats[index3];
+  index3++;
+  if(index3 >= cats.length )
+  {
+    index3=0;
+  }
+}
+
+setInterval(changeImage3, 5000);
 
 
