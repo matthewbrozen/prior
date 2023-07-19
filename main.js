be190baa92048e5607ba176f8ff9c50ea6d0d66f
Matthew Brozen <matthewbrozen@Matthews-MacBook-Pro-2.local>
2023-07-19 16:53:21 -0700

    updated gallery

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-function toggleFullScreen(id) {
-    document.getElementById(id).requestFullscreen()
-  }
+// function toggleFullScreen(id) {
+//     document.getElementById(id).requestFullscreen()
+//   }
 
-var im = document.getElementById("img");
+// var im = document.getElementById("img");
 
-var images = ["https://picsum.photos/seed/3/342/180","https://picsum.photos/seed/2/342/180","https://picsum.photos/seed/1/342/180"];
-var index=0;
+// var images = ["https://picsum.photos/seed/3/342/180","https://picsum.photos/seed/2/342/180","https://picsum.photos/seed/1/342/180"];
+// var index=0;
 
-function changeImage()
-{
-  im.setAttribute("src", images[index]);
-  index++;
-  if(index >= images.length)
-  {
-    index=0;
-  }
-}
+// function changeImage()
+// {
+//   im.setAttribute("src", images[index]);
+//   index++;
+//   if(index >= images.length)
+//   {
+//     index=0;
+//   }
+// }
 
-setInterval(changeImage, 6000);
+// setInterval(changeImage, 6000);
 
 
 
@@ -57,4 +57,209 @@ function changeImage3()
 
 setInterval(changeImage3, 5000);
 
+
+document.getElementById('s1').addEventListener('click', function(){
+
+  document.getElementById('activate1').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod1').addEventListener('click', function(){
+
+  document.getElementById('activate1').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s2').addEventListener('click', function(){
+
+  document.getElementById('activate2').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod2').addEventListener('click', function(){
+
+  document.getElementById('activate2').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+
+document.getElementById('s3').addEventListener('click', function(){
+
+  document.getElementById('activate3').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod3').addEventListener('click', function(){
+
+  document.getElementById('activate3').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s4').addEventListener('click', function(){
+
+  document.getElementById('activate4').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod4').addEventListener('click', function(){
+
+  document.getElementById('activate4').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s5').addEventListener('click', function(){
+
+  document.getElementById('activate5').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod5').addEventListener('click', function(){
+
+  document.getElementById('activate5').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s6').addEventListener('click', function(){
+
+  document.getElementById('activate6').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod6').addEventListener('click', function(){
+
+  document.getElementById('activate6').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s7').addEventListener('click', function(){
+
+  document.getElementById('activate7').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod7').addEventListener('click', function(){
+
+  document.getElementById('activate7').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s8').addEventListener('click', function(){
+
+  document.getElementById('activate8').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod8').addEventListener('click', function(){
+
+  document.getElementById('activate8').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s9').addEventListener('click', function(){
+
+  document.getElementById('activate9').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod9').addEventListener('click', function(){
+
+  document.getElementById('activate9').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s10').addEventListener('click', function(){
+
+  document.getElementById('activate10').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod10').addEventListener('click', function(){
+
+  document.getElementById('activate10').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s11').addEventListener('click', function(){
+
+  document.getElementById('activate11').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod11').addEventListener('click', function(){
+
+  document.getElementById('activate11').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s12').addEventListener('click', function(){
+
+  document.getElementById('activate12').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod12').addEventListener('click', function(){
+
+  document.getElementById('activate12').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s13').addEventListener('click', function(){
+
+  document.getElementById('activate13').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod13').addEventListener('click', function(){
+
+  document.getElementById('activate13').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s14').addEventListener('click', function(){
+
+  document.getElementById('activate14').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod14').addEventListener('click', function(){
+
+  document.getElementById('activate14').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s15').addEventListener('click', function(){
+
+  document.getElementById('activate15').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod15').addEventListener('click', function(){
+
+  document.getElementById('activate15').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s16').addEventListener('click', function(){
+
+  document.getElementById('activate16').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod16').addEventListener('click', function(){
+
+  document.getElementById('activate16').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
+
+document.getElementById('s17').addEventListener('click', function(){
+
+  document.getElementById('activate17').style.display='block';
+  document.getElementById('opacity').style.opacity='.1';
+})
+
+document.getElementById('mod17').addEventListener('click', function(){
+
+  document.getElementById('activate17').style.display='none';
+  document.getElementById('opacity').style.opacity='1';
+})
 
